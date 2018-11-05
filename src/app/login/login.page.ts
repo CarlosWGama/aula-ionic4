100d22195ab1bd40b9bef4372a836c156f46c172
Carlos W. Gama <carloswgama@gmail.com>
2018-11-05 15:36:25 -0200

    Iteração com o Layout

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  msg = "";
+  usuario: {email?:string, senha?:string} = { };
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  logar() {
+    console.log(this.usuario);
+    if (this.usuario.email == "teste@teste.com" && this.usuario.senha == "123456")
+      this.msg = "SUCESSO";
+    else
+      this.msg = "EMAIL OU SENHA ERRADA"
+  }
 }
