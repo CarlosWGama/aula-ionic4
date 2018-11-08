f410b51eccfd85279f27f969688b3cfa8b389cb7
Carlos W. Gama <carloswgama@gmail.com>
2018-11-08 15:32:51 -0200

    Toast, Loading e Alert

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Route, Router } from '@angular/router';
-import { NavController, MenuController } from '@ionic/angular';
+import { NavController, MenuController, ToastController, LoadingController, ActionSheetController, AlertController } from '@ionic/angular';
 import { AutenticacaoGuard } from '../guards/autenticacao.guard';
 
 @Component({
@@ -14,25 +14,59 @@ export class LoginPage implements OnInit {
   msg = "";
   formulario: FormGroup
 
-  constructor(private formBuilder: FormBuilder, private router: Router, private menuCtrl:MenuController) { }
+  constructor(private formBuilder: FormBuilder, private router: Router, private menuCtrl:MenuController,
+    private toastController: ToastController, private loadingController:LoadingController,
+    public alertController: AlertController) { }
 
   ngOnInit() {
     
     this.menuCtrl.enable(false);
 
     this.formulario = this.formBuilder.group({
-      email: ['valor padrão inicial', [Validators.required, Validators.email]],
+      email: ['', [Validators.required, Validators.email]],
       senha: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
 
-  logar() {
+  async cadastrar() {
+    const alert = await this.alertController.create({
+      header: 'Cadastrar-se',
+      inputs: [
+        {name: "login", type:"email", placeholder:"Digite email"},
+        {name: "senha", type: "password", placeholder: "Senha"}
+      ],
+      buttons: [
+        { text: 'Cancelar' }, 
+        { text: 'Cadastrar', handler: (data) => { 
+            console.log('Campo login: ' + data.login);
+            console.log('Campo senha: ' + data.senha);
+            
+            this.toastController.create({
+              message: "Criado com sucesso", 
+              duration: 2000
+            }).then(toast => toast.present());
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
 
+  async logar() {
+    const loading = await this.loadingController.create({
+      message: 'Carregando',
+      showBackdrop: false
+    });
+    loading.present();
     let usuario = this.formulario.value;
     if (this.formulario.valid && usuario.email == "teste@teste.com" && usuario.senha == "123456") {
       AutenticacaoGuard.podeAcessar = true;      
       this.router.navigateByUrl('/home');
     } else
-      this.msg = "EMAIL OU SENHA ERRADA"
+      this.toastController.create({
+        message: 'Login ou senha inválida',
+        duration: 2000 
+      }).then(toast => toast.present());
+    loading.dismiss();
   }
 }
